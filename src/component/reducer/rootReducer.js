df65571b503b89d367ca939aa906860cefadc5cb
monis78 <64736341+monis78@users.noreply.github.com>
2020-08-11 09:29:32 +0530

    Update rootReducer.js

--- src/component/reducer/rootReducer.js
+++ src/component/reducer/rootReducer.js
@@ -1,10 +1,9 @@
 import {combineReducers} from 'redux'
 import productReducer from './productReducer'
-import cart from './cartReducer'
 
 const rootReducer =  combineReducers({
     products:productReducer,
-    // cart:cart
+   
 })
 export default rootReducer
  
